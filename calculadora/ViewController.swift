1cf8e73dd4a76ea56112e0c1dd6ed82778109ae3
Alexei Emmanuel <alexeiemmanuel@aragon.unam.mx>
2022-12-11 13:09:16 -0600

    Se agregan las action de limpiar pantalla, punto decimal y eliminar digitos introducidos.

--- calculadora/ViewController.swift
+++ calculadora/ViewController.swift
@@ -9,36 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let ZERO:String = "0"
+    
     private var stackOfStrings = [String]()
     
     
-    
+    // UILabel para mostrar los números tecleados o el resultado de una operación
     @IBOutlet var capturaLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Inicializamos con cero la Pantalla de la calculadora
+        capturaLabel.text = self.ZERO
         
         
     }
     
     /// Evento que se dispara cuando el usuario presiona un boton de numeros.
     ///
-    /// -      Parameter sender: Button que activa el evento y que lee el número asignado.
+    /// - Parameter sender: Button que activa el evento y que lee el número asignado o el punto decimal.
     @IBAction func pressNumberButton(_ sender: UIButton) {
         let value = sender.titleLabel?.text
-        let numero = Int(value!)
-        //print(String(value: numero))
         
-        // Concatenamos el valor dellabel con el número presionado
-        self.capturaLabel.text?.append(value ?? "")
+        //let numero = Int(value!)
+        //let tagValue = sender.tag
+        //print(tagValue)
+        
+        if (self.capturaLabel.text?.first == "0"){
+            self.capturaLabel.text = ""
+        }
+        
+        // Validación para solo introducir el punto decimal una vez
+        if ((self.capturaLabel.text?.contains(".")) == false && value == "." ){
+            self.capturaLabel.text?.append(value ?? "")
+        }
+        
+        // Concatenamos solo numeros
+        if (value != "." ){
+            self.capturaLabel.text?.append(value ?? "")
+        }
+        
     }
     
-    
+    /// Action que limpia la pantalla y los operadores introducidos por el usuario
+    /// - Parameter sender: Botton AC
     @IBAction func clearCapturaLabel(_ sender: UIButton) {
-        self.capturaLabel.text = ""
+        self.capturaLabel.text = self.ZERO
         self.stackOfStrings.removeAll()
     }
     
+    
+    /// Action que elimina el ultimo digito introducido por el usuario
+    /// - Parameter sender: Botton retroceso
+    @IBAction func deleteLastCharacter(_ sender: UIButton) {
+        if(self.capturaLabel.text!.count <= 1){
+            self.capturaLabel.text = self.ZERO
+        } else {
+            self.capturaLabel.text = String(self.capturaLabel.text?.dropLast() ?? "0")
+        }
+    }
 }
 
